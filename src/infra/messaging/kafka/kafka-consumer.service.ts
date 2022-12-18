75e1d39dfea84ecb604912d2a1c38f4c540c5281
Wanuzia Braga <wanuzia.braga@grupoboticario.com.br>
2022-12-18 13:03:53 -0300

    update settings using kafka

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,27 @@
+// decorator @injectable para fazermos injeção de dependências no Nestjs
+
+import { Injectable, OnModuleDestroy } from "@nestjs/common";
+import { ServerKafka } from "@nestjs/microservices";
+
+@Injectable()
+export class KafkaConsumerService 
+extends ServerKafka 
+implements OnModuleDestroy {
+    constructor() {
+        super({
+            client: {
+                clientId: 'notification',
+                brokers: ['set-mink-5913-us1-kafka.upstash.io:9092'],
+  sasl: {
+    mechanism: 'scram-sha-256',
+    username: 'c2V0LW1pbmstNTkxMyTLJv9RUO4SSTMa0T6MInzNjdCiXwo5R9xGoGkWKwVJ78c',
+    password: '****',
+  },
+  ssl: true,
+            }
+        })
+    }
+   async onModuleDestroy() {
+        await this.close();
+    }
+}
